65919b4d44d5e37d94ba2a4bc469aacdd07c68d5
Magdy <example@example.com>
2021-07-20 15:45:27 -0400

    fix

--- client/src/pages/cliente/Necesidades.vue
+++ client/src/pages/cliente/Necesidades.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div>
-      <q-img src="nopublicidad.jpg" spinner-color="white" style="width:100%; height:200px">
+      <q-img src="nopublicidad.jpg" spinner-color="white" style="width:100%; height:160px">
         <div class="text-h6 absolute-bottom text-center bg-transparent">Solicitudes</div>
       </q-img>
     </div>
 
     <div class="q-pa-md">
       <div class="text-h6">Solicitudes activas</div>
-      <div class="text-caption text-grey-9 q-pb-md">A continuación podrás ver todas las solicitudes que has creado.</div>
-      <div class="column items-center q-gutter-sm" v-if="activas.length">
-        <q-card v-for="(item, index) in activas" :key="index" style="width:100%;height:270px;" @click="$router.push('/descripcionsolicitud/' + item._id)">
+      <div class="text-caption text-grey-9 q-pb-md">A continuación podrás ver todas las solicitudes activas que has creado.</div>
+      <div class="column items-center" v-if="activas.length">
+        <q-card class="q-mb-sm" v-for="(item, index) in activas" :key="index" style="width:100%;height:270px;" @click="$router.push('/descripcionsolicitud/' + item._id)">
             <div class="row justify-end items-center q-pa-xs">
               <div class="text-caption text-grey-8 q-pr-xs">Nivel de requerimiento</div>
               <div class="row q-gutter-xs">
@@ -20,7 +20,7 @@
               </div>
             </div>
             <div :class="'text-white q-py-xs q-px-md text-right text-bold bg-'+item.colorRadio" style="widyh:100%">{{item.name}}</div>
-            <div class="row q-py-md">
+            <div class="row q-py-sm">
               <div class="column items-center justify-center" style="width:40%">
                 <q-avatar size="90px">
                   <img :src="item.images ? baseu + '/' + item.images[0] : 'noimgpro.png'" spinner-color="white">
@@ -41,12 +41,12 @@
                 </div>
               </div>
               <div style="width:60%">
-                <div class="text-h6 q-mb-xs">{{item.name}}</div>
+                <div class="text-h6 q-mb-xs">Descripción</div>
                 <div class="row q-mb-md" style="height:50px; width:100%">
                   <div class="col-12 q-pr-xs text-grey-9 text-caption ellipsis-3-lines">{{item.descripcion}}</div>
                 </div>
                 <div class="row items-center">
-                  <div class="text-caption text-grey-9 ellipsis"><b>Estado de solicitud:</b> {{item.status}}</div>
+                  <div class="text-caption text-grey-9 ellipsis"><b>Estado de solicitud:</b> {{item.status === 0 ? 'Disponible' : 'En Progreso'}}</div>
                 </div>
                 <div class="row items-center no-wrap q-mt-xs" style="width:100%">
                   <img :src="item.categoriaInfo.icons" style="width:18px" />
@@ -65,8 +65,8 @@
     <div class="q-pa-md">
       <div class="text-h6">Solicitudes completadas</div>
       <div class="text-caption text-grey-9 q-pb-md">Historial de solicitudes realizadas y completadas con éxito.</div>
-      <div class="column items-center q-gutter-sm" v-if="completas.length">
-        <q-card v-for="(item, index) in completas" :key="index" style="width:100%;height:270px;" @click="$router.push('/descripcionsolicitud/' + item._id)">
+      <div class="column items-center" v-if="completas.length">
+        <q-card class="q-mb-sm" v-for="(item, index) in completas" :key="index" style="width:100%;height:270px;" @click="$router.push('/descripcionsolicitud/' + item._id)">
             <div class="row justify-end items-center q-pa-xs">
               <div class="text-caption text-grey-8 q-pr-xs">Nivel de requerimiento</div>
               <div class="row q-gutter-xs">
@@ -76,7 +76,7 @@
               </div>
             </div>
             <div :class="'text-white q-py-xs q-px-md text-right text-bold bg-'+item.colorRadio" style="widyh:100%">{{item.name}}</div>
-            <div class="row q-py-md">
+            <div class="row q-py-sm">
               <div class="column items-center justify-center" style="width:40%">
                 <q-avatar size="90px">
                   <img :src="item.images ? baseu + '/' + item.images[0] : 'noimgpro.png'" spinner-color="white">
@@ -97,12 +97,12 @@
                 </div>
               </div>
               <div style="width:60%">
-                <div class="text-h6 q-mb-xs">{{item.name}}</div>
+                <div class="text-h6 q-mb-xs">Descripción</div>
                 <div class="row q-mb-md" style="height:50px; width:100%">
                   <div class="col-12 q-pr-xs text-grey-9 text-caption ellipsis-3-lines">{{item.descripcion}}</div>
                 </div>
                 <div class="row items-center">
-                  <div class="text-caption text-grey-9 ellipsis"><b>Estado de solicitud:</b> {{item.status}}</div>
+                  <div class="text-caption text-grey-9 ellipsis"><b>Estado de solicitud:</b> {{'Finalizado'}}</div>
                 </div>
                 <div class="row items-center no-wrap q-mt-xs" style="width:100%">
                   <img :src="item.categoriaInfo.icons" style="width:18px" />
@@ -117,30 +117,6 @@
         <div class="text-center text-subtitle1">Actualmente sin solicitudes...</div>
       </q-card>
     </div>
-
-    <!-- <q-card class="q-pa-md bordes" v-for="(item, index) in data" :key="index" v-ripple >
-        <div class="row justify-between">
-          <div @click="$router.push('/descripcionsolicitud/' + item._id)" class="col-4">
-            <q-img :src="item.images ? baseu + '/' + item.images[0] : 'noimgpro.png'" style="width:100px; height: 80px;" />
-          </div>
-          <div @click="$router.push('/descripcionsolicitud/' + item._id)" class="col-6">
-              <q-scroll-area
-                horizontal
-                style="height: 27px"
-              >
-                <div class="text-subtitle2 text-weight-bolder">{{item.name}}</div>
-              </q-scroll-area>
-              <div class="text-caption">Caracter:</div>
-              <q-chip text-color="white" :label="item.necesidad" :color="item.necesidad === 'Urgente (1 a 3 Horas)' ? 'red' : item.necesidad === 'Medio (5 a 24 Horas)' ? 'amber-7' : 'green-14'" />
-          </div>
-          <q-separator vertical color="black" />
-          <div class="column justify-around">
-            <q-btn round flat color="white" size="sm" text-color="black" icon="edit" @click="editSolicitud(item._id)" />
-            <q-separator color="black" />
-            <q-btn round flat color="white" size="sm" text-color="red" icon="delete" @click="deleteSolicitud(item._id)" />
-          </div>
-        </div>
-      </q-card> -->
   </div>
 </template>
 
@@ -165,8 +141,8 @@ export default {
           var id = res._id
           this.$api.get('necesidad_by_user_id/' + id).then(v => {
             if (v) {
-              this.activas = v
-              this.completas = v
+              this.activas = v.filter(v => v.status === 0 || v.status === 1)
+              this.completas = v.filter(v => v.status === 2)
             }
           })
         }
