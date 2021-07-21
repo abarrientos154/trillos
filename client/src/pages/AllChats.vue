3512914f101f5de42235e104bb7edad5942b422d
josnielfermin <jfermin.wx@gmail.com>
2021-07-21 13:16:09 -0400

    avances

--- client/src/pages/AllChats.vue
+++ client/src/pages/AllChats.vue
@@ -126,25 +126,7 @@
             <q-input type="number" class="col-5" prefix="$" readonly filled dense v-model="data2.price">
             </q-input>
           </div>
-          <div class="row justify-center q-pa-sm q-mt-md">
-            <q-btn v-if="data2.status == 0 && item.status == 0" rounded  color="primary" label="Aceptar" no-caps style="width:200px" @click="acceptQuotation(data2._id)"/>
-          </div>
         </q-carousel-slide>
-        <q-carousel-slide :name="2" class="q-pa-none column items-center">
-        <div class="q-mt-xl" style="height: 200px; width: 70%;">
-          <q-img src="nopublicidad.jpg" style="height: 200px; width: 100%; border-radius: 15px">
-          <div class="absolute-full column items-center column justify-end">
-            <q-icon name="collections" class="text-grey" size="80px"></q-icon>
-            <div class="text-bold text-center text-grey">Presupuesto Aceptado</div>
-          </div>
-          </q-img>
-        </div>
-        <div class="text-h6 text-center text-bold q-mt-xl">¡Cambiaste con éxito el estado!</div>
-        <div class="text-h6 text-center text-grey-9 text-subtitle1">Podrás ver el estado de tu solicitud en tu panel de administración de solicitudes.</div>
-        <div class="q-pa-sm q-mt-md">
-          <q-btn rounded  color="primary" label="Volver" no-caps style="width:200px" @click="show = false, slide = 1"/>
-        </div>
-      </q-carousel-slide>
       </q-carousel>
     </q-dialog>
     <q-dialog v-model="show2" transition-show="slide-up" transition-hide="slide-down">
@@ -203,46 +185,6 @@
               </q-card>
             </div>
           </q-scroll-area>
-          <div v-if="change">
-            <div class="q-ml-md text-h6 text-bold q-mt-md">Cambio de estado</div>
-            <div class="q-mx-md q-mt-md">Cambia el estado de tarjeta de solicitud. Así podrás tener un control mas claro de tu trato con el cliente. Recuerda que una vez que des el trabajo por finalizado el cliente podra cambiar el estado del servicio a finalizado.</div>
-            <q-select class="q-mx-md q-mt-sm" color="grey" filled option-label="name" option-value="value" emit-value map-options v-model="form.status" :options="options" label="Estado de la solicitud" dense :error="$v.form.status.$error" error-message="Este campo es requerido" @blur="$v.form.status.$touch()" @input="extend(form.status)">
-            </q-select>
-            <div v-if="extension == true">
-              <q-input class="q-mx-md" ss filled readonly dense v-model="extensionDate" placeholder="dd/mm/aaaa" @click="$refs.qDateProxy.show()"
-              error-message="Este campo es requerido" :error="$v.extensionDate.$error" @blur="$v.extensionDate.$touch()">
-                <template v-slot:append>
-                  <q-icon name="event" class="cursor-pointer">
-                    <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-                      <q-date v-model="extensionDate" mask="DD/MM/YYYY">
-                        <div class="row items-center justify-end">
-                          <q-btn v-close-popup label="Cerrar" color="primary" flat />
-                        </div>
-                      </q-date>
-                    </q-popup-proxy>
-                  </q-icon>
-                </template>
-              </q-input>
-            </div>
-          </div>
-          <div v-if="request2.status === 1" class="row justify-center q-pa-sm q-mt-md">
-            <q-btn rounded  color="primary" label="Cambiar estado" no-caps style="width:200px" @click="change == false ? activeChange() : changeStatus()"/>
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide :name="2" class="q-pa-none column items-center">
-          <div class="q-mt-xl" style="height: 200px; width: 70%;">
-            <q-img src="nopublicidad.jpg" style="height: 200px; width: 100%; border-radius: 15px">
-            <div class="absolute-full column items-center column justify-end">
-              <q-icon name="collections" class="text-grey" size="80px"></q-icon>
-              <div class="text-bold text-center text-grey">Cambio realizado</div>
-            </div>
-            </q-img>
-          </div>
-          <div class="text-h6 text-center text-bold q-mt-xl">¡Cambiaste con éxito el estado!</div>
-          <div class="text-h6 text-center text-grey-9 text-subtitle1">Podrás ver el estado de tu solicitud en tu panel de administración de solicitudes.</div>
-          <div class="q-pa-sm q-mt-md">
-            <q-btn rounded  color="primary" label="Volver" no-caps style="width:200px" @click="$router.push('/inicio_proveedor')"/>
-          </div>
         </q-carousel-slide>
       </q-carousel>
     </q-dialog>
@@ -251,33 +193,10 @@
         <img :src="imgSelec" spinner-color="white" style="height: 100%; width: 100%" />
       </q-card>
     </q-dialog>
-    <!-- <div v-if="data.length > 0" class="q-pa-md q-gutter-md">
-        <q-card class="bordes row" @click="$router.push('/chat/' + chat._id)" v-for="(chat, index) in data" :key="index">
-            <div class="col-3">
-              <q-avatar class="q-pl-sm" size="60px">
-                <q-img :src="chat.data_request.images.length > 0 ? baseu + chat.data_request.images[0] : 'noimgpro.png'" />
-              </q-avatar>
-              <div class="text-center text-grey text-bold text-caption q-pb-xs">{{chat.status !== 'Presupuesto' ? chat.status : 'Pendiente'}}</div>
-            </div>
-            <div class="col-9">
-              <div class="text-black text-bold text-subtitle1 q-pt-sm">{{rol === 2 ? chat.data_supplier.full_name : chat.data_client.full_name + ' ' + chat.data_client.last_name}}</div>
-              <div class="row">
-              <div class="text-caption">{{chat.data_request.name}}</div>
-              </div>
-              <div class="text-caption q-px-sm">{{chat.last_message}}</div>
-              <div class="absolute-bottom text-amber-8 text-subtitle1 text-right q-pr-sm">{{chat.created_at_message}}</div>
-            </div>
-        </q-card>
-    </div>
-
-    <q-card v-else class="shadow-2 q-ma-md q-pa-md">
-      <div class="text-center text-subtitle1">Actualmente sin chats...</div>
-    </q-card> -->
   </div>
 </template>
 
 <script>
-import { required } from 'vuelidate/lib/validators'
 import env from '../env'
 export default {
   data () {
@@ -293,28 +212,13 @@ export default {
       show2: false,
       data: [],
       data2: {},
-      id: '',
       request: [],
       request2: {},
       slide: 1,
       slide2: 1,
       clientData: {},
-      categoryName: '',
-      change: false,
-      form: {},
-      options: [
-        { name: 'En progreso', value: 1 },
-        { name: 'Finalizado', value: 2 }
-      ],
-      extension: false,
-      extensionDate: ''
+      categoryName: ''
     }
-  },
-  validations: {
-    form: {
-      status: { required }
-    },
-    extensionDate: { required }
   },
   mounted () {
     this.getRecords()
@@ -355,46 +259,10 @@ export default {
       console.log('data >> ', data)
       this.idQuotation = data._id
       this.request2 = data.data_request
+      this.request2.colorRadio = this.request2.necesidad === 'Urgente (1 a 3 Horas)' ? 'red' : this.request2.necesidad === 'Medio (5 a 24 Horas)' ? 'orange' : 'blue'
       this.categoryName = this.request2.categorianame.name
       this.clientData = data.data_client
       this.show2 = true
-    },
-    async acceptQuotation (id) {
-      console.log('id :>> ', id)
-      await this.$api.put('updateQuotation/' + id).then(res => {
-        if (res) {
-          this.slide = 2
-        }
-      })
-    },
-    activeChange () {
-      if (!this.change) {
-        this.change = true
-      }
-    },
-    async changeStatus () {
-      this.$v.form.$touch()
-      if (this.form.status === 1) {
-        this.$v.extensionDate.$touch()
-        if (!this.$v.extensionDate.$error) {
-          this.form.date = this.extensionDate
-        }
-      }
-      if (!this.$v.form.$error) {
-        console.log('this.form :>> ', this.form)
-        console.log('this.id :>> ', this.id)
-        await this.$api.put('updateQuotation/' + this.idQuotation, this.form).then(res => {
-          if (res) {
-            this.slide2 = 2
-          }
-        })
-      }
-    },
-    extend (value) {
-      console.log('value :>> ', value)
-      if (value === 1) {
-        this.extension = true
-      }
     }
   },
   computed: {
